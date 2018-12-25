567980dbe627a23393ca366e454b0e75626bbc4b
Bastian Thiede <bastian_thiede@bonify.de>
2018-12-25 21:52:14 +0100

    Added docstrings

--- leet_count/count_leets.py
+++ leet_count/count_leets.py
@@ -17,25 +17,52 @@ ch.setFormatter(formatter)
 logger.addHandler(ch)
 
 
-def map_root_to_other_logs(whatsapp_logs_dict, base_file):
-    files_to_map = filter(lambda x: x != base_file, whatsapp_logs_dict)
+def map_root_to_other_logs(parsed_whatsapp_logs, base_file):
+    """
+    Combining function that filters before using the base file to find
+    mappings from a message owner in one logfile to a message owner in another
+
+    :param parsed_whatsapp_logs: (dict)
+    {'filename': (pd.DataFrame),...}
+    :param base_file:  (str) filename of base_file
+    :return: (dict)
+    {
+        (str) 'to_be_mapped_name': (str) 'basefile_message_owner',
+        ...
+    }
+    """
+    files_to_map = filter(lambda x: x != base_file, parsed_whatsapp_logs)
     files_to_map = list(files_to_map)
     logger.info('Mapping files: {}'.format(",".join(files_to_map)))
     mapping_dict = create_mapping_dict(base_file, files_to_map,
-                                       whatsapp_logs_dict)
+                                       parsed_whatsapp_logs)
     return mapping_dict
 
 
-def create_mapping_dict(base_file, files_to_map, whatsapp_logs_dict):
-    base_df = whatsapp_logs_dict[base_file]
+def create_mapping_dict(base_file, files_to_map, parsed_whatsapp_logs):
+    """
+    Function that aggregates the mapped message owners in a dictionary
+
+    :param base_file: (str) Filename of base file
+    :param files_to_map: (list) list of filenames to be mapped
+    :param parsed_whatsapp_logs: (dict)
+    {'filename': (pd.DataFrame),...}
+    :return: (dict)
+    {
+        (str) 'filename': (dict) name mapping,
+        ...
+    }
+
+    """
+    base_df = parsed_whatsapp_logs[base_file]
     mapping_dict = dict()
     for fname in files_to_map:
-        mapping = map_message_owner_names(base_df, fname, whatsapp_logs_dict)
+        mapping = map_message_owner_names(base_df, fname, parsed_whatsapp_logs)
         mapping_dict[fname] = mapping
     return mapping_dict
 
 
-def map_message_owner_names(base_df, fname, whatsapp_logs_dict):
+def map_message_owner_names(base_df, fname, parsed_whatsapp_logs):
     """
     Method to map message_owners from one file to another.
     This is a bit involved so let me explain my reasoning here:
@@ -53,11 +80,11 @@ def map_message_owner_names(base_df, fname, whatsapp_logs_dict):
 
     :param base_df: (pd.DataFrame) base dataframe used for mapping
     :param fname: filename of base file
-    :param whatsapp_logs_dict: complete data dict containing all logs
+    :param parsed_whatsapp_logs: complete data dict containing all logs
     :return: (dict) mapping from one contact to another
     """
     logger.info('Finding Conversion for: {}'.format(fname))
-    map_df = whatsapp_logs_dict[fname]
+    map_df = parsed_whatsapp_logs[fname]
 
     merged_dfs = pd.merge(base_df, map_df,
                           on=['message_text', 'day', 'month', 'year'])
@@ -73,25 +100,47 @@ def map_message_owner_names(base_df, fname, whatsapp_logs_dict):
     logger.info('Created conversion: {}'.format(mapping))
     return mapping
 
-def map_names(whatsapp_logs_dict):
-    base_file = find_root_mapping_file(whatsapp_logs_dict)
+
+def map_names(parsed_whatsapp_logs):
+    """
+    Function that maps message_owners in all logfiles to the same
+    message owner
+    :param parsed_whatsapp_logs: (dict)
+    {'filename': (pd.DataFrame),...}    :return:
+    """
+    base_file = find_root_mapping_file(parsed_whatsapp_logs)
     logger.info('Using {} as mapping root'.format(base_file))
-    mapping = map_root_to_other_logs(whatsapp_logs_dict, base_file)
+    mapping = map_root_to_other_logs(parsed_whatsapp_logs, base_file)
     for key in mapping:
         conversion = mapping[key]
-        df = whatsapp_logs_dict[key]
+        df = parsed_whatsapp_logs[key]
         df['message_owner'] = df['message_owner'].apply(conversion.get)
-        whatsapp_logs_dict[key] = df
+        parsed_whatsapp_logs[key] = df
         logger.info('Converted names in file: {}'.format(key))
     return mapping
 
 
-def find_root_mapping_file(whatsapp_logs_dict):
+def find_root_mapping_file(parsed_whatsapp_logs):
+    """
+    Function to determine which file to use as a mapping root for all
+    names. The logic here is as follows, a file is a good candidate
+    if it doesn't contain a lot of names that do no contain any
+    numbers (i.e.: phone numbers) and additionally have a ratio between
+    all unique users and valid user names (those without numbers) close
+    to one. The reason for this is that some people might switch
+    numbers which would increase the number of unique users, the utv-ratio
+    counteracts this effect.
+
+    :param parsed_whatsapp_logs: (dict)
+    {'filename': (pd.DataFrame),...}
+
+    :return: (str) basefile name
+    """
     max_uniques = 0
     utv_ratio = 0
     base_file = None
-    for key in whatsapp_logs_dict:
-        unique_owners = whatsapp_logs_dict[key]['message_owner'].unique()
+    for key in parsed_whatsapp_logs:
+        unique_owners = parsed_whatsapp_logs[key]['message_owner'].unique()
         valid_names = [x for x in unique_owners
                        if not any(c.isdigit() for c in x)]
         valid_users = len(valid_names)
@@ -110,10 +159,25 @@ def find_root_mapping_file(whatsapp_logs_dict):
     return base_file
 
 
-def get_section_winners(day, month, year, parsed_files_dict, feat_key):
+def get_section_winners(day, month, year, parsed_whatsapp_logs, feat_key):
+    """
+    Calculates the winners of a certain leet/zeet time accross all logs.
+    Also solves disputes between mutliple logfile. i.E.: If a zeet is only
+    determined as a win if the majority of the logfiles says so.
+    If it's a 50:50 split the zeet is counted as a win.
+    
+    :param day:  (int)
+    :param month:  (int)
+    :param year:  (int)
+    :param parsed_whatsapp_logs:(dict)
+    {'filename': (pd.DataFrame),...} 
+    :param feat_key: (str) determining which feature should be evaluated
+    (feasible for is_zeet and is_leet)
+    :return: (list) list of winners
+    """
     scaffold_df = pd.DataFrame()
-    for key in parsed_files_dict:
-        file_df = parsed_files_dict[key]
+    for key in parsed_whatsapp_logs:
+        file_df = parsed_whatsapp_logs[key]
         mask = get_date_mask(day, file_df, month, year)
         sel_df = file_df[mask]
         occ_count = (sel_df
@@ -129,10 +193,23 @@ def get_section_winners(day, month, year, parsed_files_dict, feat_key):
     return winners
 
 
-def get_winner_420(day,month,year,parsed_files_dict):
+def get_winner_420(day, month, year, parsed_whatsapp_logs):
+    """
+    Special method to determine a 420 message win. Since 420 is possible
+    at multiple times it's a little more involved to calculate.
+    First determine a winner for 16:20 and 4:20 then aggregate
+
+    :param day: (int)
+    :param month: (int)
+    :param year: (int)
+    :param parsed_whatsapp_logs:(dict)
+    {'filename': (pd.DataFrame),...}
+    :return: (list) list of winners, if someone has won both 4:20 and 16:20
+    the person will appear twice in the list
+    """
     scaffold_df = pd.DataFrame()
-    for key in parsed_files_dict:
-        file_df = parsed_files_dict[key]
+    for key in parsed_whatsapp_logs:
+        file_df = parsed_whatsapp_logs[key]
         mask = get_date_mask(day, file_df, month, year)
         mask_16 = mask & (file_df.hour == 16)
         mask_04 = mask & (file_df.hour == 4)
@@ -149,6 +226,15 @@ def get_winner_420(day,month,year,parsed_files_dict):
 
 
 def calc_occurences_420(file_df, mask_04, mask_16):
+    """
+    Calculate the occurences for 16:20 and 4:20
+    :param file_df: (pd.DataFrame) dataframe of messages
+    :param mask_04: (pd.Series of bools) mask to select 4:20 users on a
+    certain day
+    :param mask_16: (pd.Series of bools) mask to select 16:20 users
+    on a certain day
+    :return:(pd.Series) Series of Occurences indexed by message owner
+    """
     occ_count_04 = (file_df[mask_04]
                     .groupby('message_owner')
                     .is_420
@@ -167,10 +253,6 @@ def calc_occurences_420(file_df, mask_04, mask_16):
     occs_420 = scaff.sum(axis=1)
     return occs_420
 
-
-
-
-
 def get_date_mask(day, file_df, month, year):
     mask = ((file_df.day == day)
             & (file_df.month == month)
@@ -179,6 +261,21 @@ def get_date_mask(day, file_df, month, year):
 
 
 def count_leet_and_greet(data_folder, start='01-01-2018', end='12-31-2018'):
+    """
+    Aggregating function to  count leets and greets over the course of a
+    given time range. Iterates on timerange calculates winner for each
+    day.
+
+    :param data_folder: Folder to parse data from
+    :param start: (str) time to start (american time format)
+    :param end: (str) time to end (american time format)
+    :return: (dict) Dict of aggregated statistics(count) for zeet and greet
+    {
+        'zeet': (dict) dict with counts per message owner,
+        'leet': (dict) dict with counts per message owner,
+        'fourtwenty': : (dict) dict with counts per message owner
+    }
+    """
     parsed_files_dict = parse_log_files(data_folder)
     logger.info('Mapping message owners between files')
     map_names(parsed_files_dict)
@@ -193,7 +290,7 @@ def count_leet_and_greet(data_folder, start='01-01-2018', end='12-31-2018'):
         day = date.day
         winner_partial = lambda x: get_section_winners(
             day=day, month=month, year=year,
-            parsed_files_dict=parsed_files_dict,
+            parsed_whatsapp_logs=parsed_files_dict,
             feat_key=x
         )
 
@@ -210,6 +307,16 @@ def count_leet_and_greet(data_folder, start='01-01-2018', end='12-31-2018'):
 
 
 def parse_log_files(data_folder):
+    """
+    Function to parse all data from the specified data folder
+
+    :param data_folder: (str) dir-path containing all logs
+    :return: (dict) dict of filename to parsed data mapping
+    {
+        'fname': (pd.DataFrame),
+        ...
+    }
+    """
     parsed_files = dict()
     logger.info('Reading from: {}'.format(data_folder))
     data_files = [x for x in os.listdir(data_folder) if x.endswith('.txt')]
--- leet_count/extractor.py
+++ leet_count/extractor.py
@@ -5,6 +5,13 @@ import pandas as pd
 
 
 def parse_datetime(some_str):
+    """
+    Pasing timestamps in logs, since multiple formats need to be handled
+    this method tries a couple options to parse.
+
+    :param some_str: (str) whatsapplog line to parse
+    :return: datetime or None (if no formatting succeeeded)
+    """
     formats = [(r'\d{2}\.\d{2}\.\d{2}, \d{2}:\d{2}:\d{2}','%d.%m.%y, %H:%M:%S'),
                (r'\d{1,2}/\d{1,2}/\d{1,2}, \d{2}:\d{2}', '%m/%d/%y, %H:%M'),
                (r'\d{2}\.\d{2}\.\d{2}, \d{2}:\d{2}', '%d.%m.%y, %H:%M')]
@@ -12,15 +19,19 @@ def parse_datetime(some_str):
     for regex,date_format in formats:
         date_match = re.search(regex, some_str)
         if date_match:
-            try:
-                date = datetime.strptime(date_match.group(), date_format)
-            except Exception:
-                a = 1+1
+            date = datetime.strptime(date_match.group(), date_format)
 
     return date
 
 
 def parse_owner_android(some_str):
+    """
+    Method that tries to parse the message owner of a log line,
+    needs to be also specific to system logs have been generated.
+
+    :param some_str: (str) Whatsapp log line
+    :return: (str) owner of message or None
+    """
     regex = r' - .+: '
     name_match = re.search(regex, some_str)
     if name_match:
@@ -33,6 +44,11 @@ def parse_owner_android(some_str):
 
 
 def parse_owner(some_str):
+    """
+    Grouping function to parse either android or ios whatsapp logs
+    :param some_str:  (str) Whatsapp log line
+    :return: (str) owner of message or None
+    """
     name_filter = parse_owner_ios(some_str)
     if name_filter is None:
         name_filter = parse_owner_android(some_str)
@@ -40,6 +56,13 @@ def parse_owner(some_str):
 
 
 def parse_owner_ios(some_str):
+    """
+    Method that tries to parse the message owner of a log line,
+    needs to be also specific to system logs have been generated.
+
+    :param some_str:  (str) Whatsapp log line
+    :return: (str) owner of message or None
+    """
     regex = r'] .+: '
     name_match = re.search(regex, some_str)
     if name_match:
@@ -52,10 +75,24 @@ def parse_owner_ios(some_str):
 
 
 def parse_text(some_str):
+    """
+    Tries to parse message content, not used for counting only used
+    for matching message owners accross multiple logs
+
+    :param some_str: (str) Whatsapp log line
+    :return:
+    """
     return some_str.split(': ')[-1].replace('\n', '')
 
 
 def parse_details(log_line):
+    """
+    Function that parses date, message owner and message content from
+    a whatsapp logline
+    :param log_line:  (str) Whatsapp log line
+    :return: (dict) parsed message details or None
+    """
+
     try:
         message_time = parse_datetime(log_line)
         message_owner = parse_owner(log_line)
@@ -110,6 +147,19 @@ def determine_fail(detail_dict):
 
 
 def build_leet_features(detail_dict):
+    """
+    function creating message features (either leet,zeet,420 soli or fail)
+    currently soli and fail are not processed by the aggregation script
+
+    :param detail_dict: (dict) message details (time,owner,text)
+    {
+        'message_time':(datetime),
+        'message_text': (str),
+        'message_owner' (str)
+    }
+
+    :return: (dict) feature dict of notable events
+    """
     is_leet = determine_leet(detail_dict)
     is_zeet = determine_zeet(detail_dict)
     is_420 = determine_420(detail_dict)
@@ -121,6 +171,26 @@ def build_leet_features(detail_dict):
 
 
 def parse_file_to_leet(file_content):
+    """
+    Method that parses file content (str) of a whatsapp log.
+    First gets message details and if that succeedes calculates
+    relevant events
+
+    :param file_content:  (str) compelete Whatsapp log line
+    :return: (list): list containing a dictionary for every log line
+    [
+        {
+         'message_text':(str),
+         'message_time':(datetime),
+         'message_owner': (str),
+         'is_leet':(int) 1 or 0,
+         'is_zeet':(int) 1 or 0,
+         'is_420':(int) 1 or 0,
+         'is_soli': (int) 1 or 0,
+         'is_fail': (int) 1 or 0,
+        },...
+    ]
+    """
     data = list()
     for line in file_content.split('\n'):
         details = parse_details(line)
