3267c78d17adb45c926b0ce676efb938e0eac3f9
(BT) Bastian Thiede <bt@neurocat.ai>
2020-12-29 00:46:47 +0100

    Added setup py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='leet_count',
+    version='0.1',
+    packages=['leet_count'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
